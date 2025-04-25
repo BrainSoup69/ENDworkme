7989255abc5864ce468cb76ea75d63ec7c95bb15
Caramen <teaaddict169@gmail.com>
2025-04-25 11:38:39 +0200

    UI_Loading_Screens

---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenManager : MonoBehaviour
+{
+    public CanvasGroup loadingScreen;
+    public CanvasGroup introScreen;
+    public float fadeSpeed = 1f;
+    public float loadingDuration = 2f;
+
+    private void Start()
+    {
+        StartCoroutine(HandleIntroSequence());
+    }
+
+    IEnumerator HandleIntroSequence()
+    {
+
+        loadingScreen.alpha = 1f;
+        loadingScreen.blocksRaycasts = true;
+
+ 
+        yield return new WaitForSeconds(loadingDuration);
+
+        yield return StartCoroutine(FadeCanvasGroup(loadingScreen, 1f, 0f));
+
+        loadingScreen.blocksRaycasts = false;
+
+        introScreen.alpha = 0f;
+        introScreen.blocksRaycasts = true;
+        yield return StartCoroutine(FadeCanvasGroup(introScreen, 0f, 1f));
+    }
+
+    IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end)
+    {
+        float elapsed = 0f;
+        while (elapsed < 1f)
+        {
+            elapsed += Time.deltaTime * fadeSpeed;
+            cg.alpha = Mathf.Lerp(start, end, elapsed);
+            yield return null;
+        }
+        cg.alpha = end;
+    }
+}
